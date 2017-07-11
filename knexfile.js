36f0a06728870871bf790aae7e862138e8355322
Alex Read <alexread730@gmail.com>
2017-07-11 15:57:33 -0600

    add production db connection

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/gReads'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 
 };
