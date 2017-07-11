0cdd454b730f3e36af91df2bcd47077a45b484bf
Alex Read <alexread730@gmail.com>
2017-07-11 15:00:40 -0600

    added joins w/ author

--- api/books.js
+++ api/books.js
@@ -8,11 +8,37 @@ function isValidId(req, res, next) {
   next(new Error('Invalid ID'));
 }
 
+function makeBookArray(books) {
+  const collectionOfBooks = [];
+  const booksWithAuthors = {};
+  books.forEach(book => {
+    console.log(book);
+    if (!booksWithAuthors[book.book_id]) {
+      const bookInstance = {
+        id: book.book_id,
+        title: book.title,
+        authors: [],
+        genre: book.genre,
+        description: book.description,
+        cover_url: book.cover_url
+      };
+      collectionOfBooks.push(bookInstance);
+      booksWithAuthors[book.book_id] = bookInstance;
+    }
+    booksWithAuthors[book.book_id].authors.push({
+      firstName: book.firstName,
+      lastName: book.lastName
+    })
+  })
+  return booksWithAuthors;
+}
+
 router.get('/', (req, res) => {
   queries.getAllBooks()
     .then(books => {
-      res.json(books);
+      res.json(makeBookArray(books));
     });
 });
+
 
 module.exports = router;
--- db/book-queries.js
+++ db/book-queries.js
@@ -3,6 +3,9 @@ const knex = require('./knex');
 module.exports = {
   //  Books
   getAllBooks: () => {
-    return knex('book');
+    return knex('author_book')
+              .join('author', 'author.id', 'author_id')
+              .join('book', 'book.id', 'book_id')
+              .select('book_id','title', 'genre', 'firstName', 'lastName', 'description', 'cover_url');
   }
 }
